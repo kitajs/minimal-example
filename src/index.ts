6dd58667b8634c27e232e4fb356505ad6fc79530
Arthur Fiorette <me@arthur.place>
2023-10-15 22:50:16 -0300

    feat: error example

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,20 @@ import fastify from "fastify";
 import { Kita } from "@kitajs/runtime";
 import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerUi from "@fastify/swagger-ui";
+import fastifySensible from "@fastify/sensible";
 
 const app = fastify();
 
-// Simply register fastify's swagger support
+// Register fastify sensible to allow error handling
+app.register(fastifySensible, {
+  sharedSchemaId: "HttpError",
+});
+
+// Registers fastify's swagger support
 app.register(fastifySwagger);
 app.register(fastifySwaggerUi);
 
-// This registers the generated kita plugin
+// Registers the generated kita plugin
 app.register(Kita);
 
 // Starts your server and prints out the port
