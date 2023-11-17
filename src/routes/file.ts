085b28cee8ee5b50f118bc583f47112b87cca5ab
Arthur Fiorette <me@arthur.place>
2023-11-17 18:59:17 -0300

    feat: return file

--- src/routes/file.ts
+++ src/routes/file.ts
@@ -1,10 +1,12 @@
 import { File, SavedFile } from '@kitajs/runtime';
+import { FastifyReply } from 'fastify';
 
 /**
  * @tag File
  * @operationId returnInMemoryFile
  */
-export async function post(file: File) {
+export async function post(file: File, reply: FastifyReply) {
+  reply.type(file.mimetype);
   return (await file.toBuffer()).toString();
 }
 
